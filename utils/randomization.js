5170497d8d64c07a3f624c41b2a0781d0d3189e0
alex-ten <tenalexander1991@gmail.com>
2022-08-23 12:02:54 +0200

    Split randomization and stimulus generation functions

--- utils/randomization.js
+++ utils/randomization.js
@@ -1,23 +1,19 @@
 // Randomly shuffle an array (source: https://javascript.info/task/shuffle)
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
-  
-      // swap elements array[i] and array[j]
-      // we use "destructuring assignment" syntax to achieve that
-      // you'll find more details about that syntax in later chapters
-      // same can be written as:
-      // let t = array[i]; array[i] = array[j]; array[j] = t
-      [array[i], array[j]] = [array[j], array[i]];
+        let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+
+        // swap elements array[i] and array[j]
+        // we use "destructuring assignment" syntax to achieve that
+        // you'll find more details about that syntax in later chapters
+        // same can be written as:
+        // let t = array[i]; array[i] = array[j]; array[j] = t
+        [array[i], array[j]] = [array[j], array[i]];
     }
+    return array;
   };
 
 
 function randInt(max) {
-  return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max);
 }
-
-// function add a function to output a random label of a stimulus exemplar from a given family (depending on categorization rule)
-function getFeatures() {
-
-};
