edf8dcc3fdc8b4d29f88ce5356338dcab15bfeae
alex-ten <tenalexander1991@gmail.com>
2022-08-23 12:02:27 +0200

    Add stimulus generation functions

---
+++ utils/stim_gen.js
@@ -0,0 +1,35 @@
+// Generate stimulus features based on array that specifies whether a dimension is variable or fixed at a given value
+function genStimFeatures(dimVariance) {
+    let features  = []
+    for (let i = 0; i < dimVariance.length; i++) {
+        if (dimVariance[i] === null) {
+            features.push(randInt(6) + 1)
+        };
+    }
+    return features
+};
+
+
+// Return only relevant features based on the (boolean) relevance array
+function getRelevantFeatures(features, relevance) {
+    return features.filter((item, i) => relevance[i])
+}
+
+
+function xnor(x, y) {
+    return (x && y) || (!x && !y)
+}
+
+
+// Get stimulus category based on the values of relevant features
+function getStimCat(relDims) {
+    if (relDims.length == 0) {
+        // R rule: category is random
+        return randInt(2)
+    } else if (relDims.length == 1) {
+        // 1D and i1D rule: category based on 1 relevant dim
+        return relDims[0] < 3 ? 1 : 0
+    } else if (relDims.length == 2) {
+        return xnor(relDims[0] < 3, relDims[1] < 3) ? 1 : 0
+    }
+}
