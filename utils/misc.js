38fa39d6f80cfc01bc616991c7f4ed46bd48b895
alex-ten <tenalexander1991@gmail.com>
2022-09-06 16:35:52 +0200

    We can now add and parameterize overlay

--- utils/misc.js
+++ utils/misc.js
@@ -1,3 +1,18 @@
+function revealParentDiv() {
+    jatos.removeOverlay()
+    $(".hidden").css("display", "block")
+}
+
+function getOverlaySetting(s) {
+    if ('overlay' in jatos.componentJsonInput) {
+        if (s in jatos.componentJsonInput.overlay) {
+            return jatos.componentJsonInput.overlay[s]
+        }
+    } else {
+        return jatos.studyJsonInput.default.overlay[s]
+    }
+}
+
 function incrementStageTrialCount() {
         jatos.studySessionData.stageTrialsComplete += 1
     $("#tcount").prop({
@@ -8,7 +23,7 @@ function incrementStageTrialCount() {
 function addCustomAbortButton() {
     jatos.addAbortButton({
         text: "Terminate",
-        confirmText: "You are trying to terminate the experiment prematurely. Press OK to confirm or press Cancel to go back to the experiment.",
+        confirmText: "You are about to terminate the experiment before completing it. Press OK to confirm or press Cancel to go back to the experiment.",
         tooltip: "This button terminates the experiment.",
         msg: `${jatos.workerId} dropped out early.`,
         style: ""
