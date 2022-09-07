e9708e996d17bc83cd96dd8e89c0eded49cdaaa8
alex-ten <tenalexander1991@gmail.com>
2022-09-07 11:30:33 +0200

    Clean up

--- utils/circle-layout.js
+++ utils/circle-layout.js
@@ -1,8 +1,7 @@
 // Credit to ThiefMaster: https://stackoverflow.com/questions/10152390/dynamically-arrange-some-elements-around-a-circle
 function distributeChoices() {
     let fields = $('.option')
-    let container = $('#menu-square')
-    let sidelength = container.height()
+    let sidelength = $('#menu-square').height()
     let radius = .25 * sidelength;
     let angle = 0
     let step = (2 * Math.PI) / fields.length
@@ -10,9 +9,6 @@ function distributeChoices() {
     fields.each(function() {
         let x = Math.round(sidelength/2 + radius * Math.cos(angle) - $(this).outerWidth()/2);
         let y = Math.round(sidelength/2 + radius * Math.sin(angle) - $(this).outerHeight()/2);
-        if(window.console) {
-            console.log($(this).text(), x, y);
-        }
         $(this).css({
             left: x + 'px',
             top: y + 'px'
